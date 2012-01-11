c77ea8f12c217bc8f6b99a491abf6cdcf8726f2b
Jason Ardell <ardell@gmail.com>
2012-01-11 13:34:47 -0600

    Start Planbox' timer when starting a story. Ask which task to start if there is more than one task in the story.

--- lib/GitPlanbox/Start.php
+++ lib/GitPlanbox/Start.php
@@ -46,10 +46,8 @@ class GitPlanbox_Start extends CLIMax_BaseCommand
     exec($command, $output, $returnCode);
     if ($returnCode !== 0) throw new Exception("Error creating new branch.");
 
-    // @TODO later...
-    //    If there is only one task, start the timer for that task
-    //    Otherwise aask which task they want to start
-    //    Tell the user that the task's timer has been started
+    // Start timer
+    $this->_startTimer($session, $storyId);
 
     // Success!
     return 0;
@@ -73,6 +71,56 @@ class GitPlanbox_Start extends CLIMax_BaseCommand
     }
 
     return $branchTemplate;
+  }
+
+  private function _startTimer($session, $storyId)
+  {
+    // Fetch the story
+    $postData = array('story_id' => $storyId);
+    $story  = $session->post('get_story', $postData);
+
+    // Don't do anything if there are no tasks
+    if (count($story->tasks) < 1)
+    {
+      print("Not starting timer because story {$storyId} has no tasks.");
+      return;
+    }
+
+    // If we have only one task, start the timer for that task
+    if (count($story->tasks) == 1)
+    {
+      $task = array_shift($story->tasks);
+      $this->_startTimerForTask($session, $storyId, $task->id);
+      return;
+    }
+
+    // Otherwise, ask the user which task they want to start
+    $tasksByTaskId = array();
+    foreach ($story->tasks as $task)
+    {
+      $tasksByTaskId[$task->id] = $task;
+      printf("%8s %10s - %-50s\n", "#{$task->id}", $task->status, $task->name);
+    }
+    $taskId = intVal(GitPlanbox_Util::readline("Which task would you like to work on? "));
+    if (!isset($tasksByTaskId[$taskId]))
+    {
+      throw new Exception("Invalid task id {$taskId} for story {$storyId}, expected one of: " . implode(', ', array_keys($tasksByTaskId)) . '.');
+    }
+    $this->_startTimerForTask($session, $storyId, $taskId);
+  }
+
+  private function _startTimerForTask($session, $storyId, $taskId)
+  {
+    if (!$storyId) throw new Exception("Expected storyId, got " . var_export($storyId, true));
+    if (!$taskId) throw new Exception("Expected taskId, got " . var_export($taskId, true));
+
+    $postData = array(
+                  'story_id' => $storyId,
+                  'task_id'  => $taskId,
+                  'status'  => 'inprogress',
+                );
+    $session->post('update_task', $postData);
+    print("Started timer for story #{$storyId}, task #{$taskId}.\n");
   }
 
 }
