063445c4c4ea059abfb8acaa0b382c39c93e4337
Jason Ardell <ardell@gmail.com>
2012-01-11 15:19:35 -0600

    Tell the user how many stories were skipped because of status=completed/rejected/etc in git-planbox list.

--- lib/GitPlanbox/List.php
+++ lib/GitPlanbox/List.php
@@ -15,13 +15,28 @@ class GitPlanbox_List extends CLIMax_BaseCommand
     $stories  = $session->post('get_stories', $postData);
 
     // Format stories nicely
+    $skippedStoriesByStatus = array();
     foreach ($stories as $story)
     {
       // Skip stories that are done
       $statusesToSkip = array('completed', 'delivered', 'accepted', 'rejected', 'released', 'blocked');
-      if (in_array($story->status, $statusesToSkip)) continue;
+      if (in_array($story->status, $statusesToSkip))
+      {
+        if (!isset($skippedStoriesByStatus[$story->status])) $skippedStoriesByStatus[$story->status] = 0;
+        $skippedStoriesByStatus[$story->status]++;
+        continue;
+      }
 
       printf("%8s %9s %10s - %-50s\n", "#{$story->id}", $story->type, $story->status, $story->name);
+    }
+    if (!empty($skippedStoriesByStatus))
+    {
+      $skippedStoryStatuses = array();
+      foreach ($skippedStoriesByStatus as $status => $numSkipped)
+      {
+        array_push($skippedStoryStatuses, "{$numSkipped} {$status}");
+      }
+      print("...plus " . implode(', ', $skippedStoryStatuses) . " tasks that are not displayed.\n");
     }
 
     return 0;
